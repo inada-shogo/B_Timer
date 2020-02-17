5b5a41e8a289c612b2167cea18f732d11dde8d79
稲田笙吾 <55049633+inada-shogo@users.noreply.github.com>
2020-02-17 22:32:57 +0900

    Add files via upload

---
+++ webapp/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+    
+    <link rel ="stylesheet" type="text/css" href="css/style.css">
+    <link rel ="stylesheet" type="text/css" href="css/menu.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    
+  <head>
+    <meta charset="utf-8">
+    <title>Airtist-Research(アーティストリサーチ)</title>
+  </head>
+    
+    
+    
+    
+  <body>
+      
+      <nav class="menu">
+   <input type="checkbox" href="#" class="menu-open" name="menu-open" id="menu-open" />
+   <label class="menu-open-button" for="menu-open">
+    <span class="lines line-1"></span>
+    <span class="lines line-2"></span>
+    <span class="lines line-3"></span>
+  </label>
+
+   <a href="ranking.html" class="menu-item blue"> <i class="fa fa-anchor"></i> </a>
+   <a href="help.html" class="menu-item green"> <i class="fa fa-coffee"></i> </a>
+   <a href="#" class="menu-item red"> <i class="fa fa-heart"></i> </a>
+   <a href="#" class="menu-item purple"> <i class="fa fa-microphone"></i> </a>
+   <a href="#" class="menu-item orange"> <i class="fa fa-star"></i> </a>
+   <a href="#" class="menu-item lightblue"> <i class="fa fa-diamond"></i> </a>
+</nav>
+
+      <div class = "outer">
+          <div class="inner">
+              <h1>Airtist Reseach</h1>
+              <form method="post" action="result.php" class="search_container">
+                  <input type="search" name="keyword">
+    <input type="submit" size="22" placeholder="　歌手名を検索" name="submit" value="&#xf002">	
+                  
+              </form>
+          </div>
+      </div>
+      
+         
+  </body>
+</html>
---
+++ webapp/result.php
@@ -0,0 +1,58 @@
+<?php
+
+$user = 'ar99_admin';
+$pass = 'Shogo1231';
+
+try{
+ 
+  $db = new PDO(
+      'mysql:host=mysql8086.xserver.jp;dbname=ar99_music',$user,$pass);
+
+  $keyword = $_POST["keyword"];
+ 
+} catch(PDOException $e) {
+  die('エラーメッセージ：'.$e->getMessage());
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+    
+  <head>
+    <meta charset="utf-8">
+    <title>Airtist-Research(アーティストリサーチ)</title>
+  </head>
+  
+<aside class="profile-card">
+
+    <link rel ="stylesheet" type="text/css" href="css/profile.css">
+    
+    <header><h1>検索結果</h1></header>
+    
+      
+
+    <!-- the username -->
+    <?php
+
+$keyword = '%'.$keyword.'%';
+//二つのカラムを同時に検索したい場合は以下
+$stmt = $db->prepare("SELECT * FROM airtist_name WHERE name1 LIKE :keyword OR name2 LIKE :keyword");
+$stmt->bindParam( ':keyword' ,$keyword, PDO::PARAM_STR);
+$stmt->execute();
+
+      
+//検索結果表示
+while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+    
+    
+        print '<a href = "https://www.youtube.com/results?search_query='.htmlspecialchars($result['name1'],ENT_QUOTES,'UTF-8').'">'.htmlspecialchars($result['name1'],ENT_QUOTES,'UTF-8').'</a></br>';
+        
+        print '<a href = "https://www.youtube.com/results?search_query='.htmlspecialchars($result['name2'],ENT_QUOTES,'UTF-8').'">'.htmlspecialchars($result['name2'],ENT_QUOTES,'UTF-8').'</a></br>';
+        
+}
+
+
+?>
+      
+      
+    </aside>
